19e8ea5c033e80b58e3da4c3b58f5daf3038d93e
nikhilcm <rose12nikhl@gmail.com>
2017-07-03 23:55:29 +0530

    cli change

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var express = require("express"),
 app.set('views', path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
@@ -23,5 +23,5 @@ require("./routes/routes.js")(express, app);
 var port = process.env.PORT || 3000 ;
 
 app.listen(port, function () {
-    console.log("App running at:"+port)
+    console.log("browse at http://localhost:"+port+"/")
 });
